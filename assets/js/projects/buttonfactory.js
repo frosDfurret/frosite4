d12a1573ab7ece643794f8e63a8e9e34ae768fa2
frosDfurret <64030476+frosDfurret@users.noreply.github.com>
2021-12-13 23:19:36 +0000

    FIXED THE STUPID SCREENSHOT THING

--- assets/js/projects/buttonfactory.js
+++ assets/js/projects/buttonfactory.js
@@ -1,6 +1,7 @@
-const projectTitles = ["aestheticlock", "anonet","WHS Hack Club"];
-const projectHovers = ["A clock, but it's a e s t h e t i c ? (i hate that term)", "Unblock the net. Using proxies from TitaniumNetwork","The site I made for the WHS Hack Club."];
-const projectURL = ["https://aestheticlock.glitch.me", "https://anonet.frosdfurret.xyz","https://whs-hackclub.frosdfurret.xyz/home"];
+const projectTitles = ["aestheticlock", "anonet","WHS Hack Club","Google"];
+const projectHovers = ["A clock, but it's a e s t h e t i c ? (i hate that term)", "Unblock the net. Using proxies from TitaniumNetwork","The site I made for the WHS Hack Club.","mine i swear"];
+const projectURL = ["https://aestheticlock.glitch.me", "https://anonet.frosdfurret.xyz","https://whs-hackclub.frosdfurret.xyz","https://www.google.com"];
+const thumbURL = ["https://aestheticlock.glitch.me", "https://6c6f6e676c697665696e7465726e657466726565646f6d.frosdfurret.repl.co/","https://whs-hackclub.frosdfurret.xyz/home","https://www.google.com"];
 const projectHeight = Math.round(window.innerHeight / 6)
 const projectWidth = Math.round(window.innerWidth / 1.2)
 
@@ -11,11 +12,10 @@ for (var i = 0; i < projectTitles.length; i++) {
   newPB.innerHTML = projectTitles[i];
   newPB.title = projectHovers[i]
   newPB.setAttribute('onclick', "javascript:window.location='" + projectURL[i] + "';")
-  newPB.style.backgroundImage = "url('https://api.apiflash.com/v1/urltoimage?access_key=6381b01dc6194d33b030b47cf0a3165e&url=" + projectURL[i] + "&height=" + projectHeight * 3 + "&width=" + projectWidth * 3 + "')"
+  newPB.style.backgroundImage = "url('https://gimedascarnshote.herokuapp.com/capture?url=" + thumbURL[i] + "&height=" + projectHeight * 3 + "&width=" + projectWidth * 3 + "&transparent=true')"
   newPB.setAttribute('onmouseover', "javascript:blowup('" + i + "')")
   newPB.setAttribute('onmouseleave', "javascript:nowup('" + i + "')")
   newPB.style.backgroundSize = "cover";
-  newPB.style.backgroundColor = "black";
   var brak = document.createElement("br")
   document.body.appendChild(newPB)
   document.body.appendChild(brak)
@@ -23,19 +23,19 @@ for (var i = 0; i < projectTitles.length; i++) {
 
 projectbutton = document.getElementsByClassName('projectbutton')
 for (var i = 0; i < projectbutton.length; i++) {
-  projectbutton[i].style.width = window.innerWidth / 1.2 + "px"
-  projectbutton[i].style.height = window.innerHeight / 6 + "px"
-  projectbutton[i].style.fontSize = window.innerHeight / 13 + "px"
-  projectbutton[i].style.marginBottom = window.innerHeight / 35 + "px"
+  projectbutton[i].style.width = window.innerWidth - window.innerHeight / 15 + "px"
+  projectbutton[i].style.height = window.innerHeight / 8 + "px"
+  projectbutton[i].style.fontSize = window.innerHeight / 14 + "px"
+  projectbutton[i].style.marginBottom = 1 + "px"
 }
 
 function blowup(e) {
   /*
   projectbutton[e].style.width = window.innerWidth / 1.2 + 50 + "px"
   projectbutton[e].style.height = window.innerHeight / 6 + 50 + "px"*/
-  projectbutton[e].style.fontSize = window.innerHeight / 13 + 10 + "px"
+  projectbutton[e].style.fontSize = window.innerHeight / 14 + 10 + "px"
 }
 
 function nowup(e) {
-  projectbutton[e].style.fontSize = window.innerHeight / 13 + "px"
+  projectbutton[e].style.fontSize = window.innerHeight / 14 + "px"
 }
--- assets/js/projects/resize.js
+++ assets/js/projects/resize.js
@@ -1,7 +1,16 @@
-title = document.getElementsByTagName('h1')[0]
+//title = document.getElementsByTagName('h1')[0]
+backbutton = document.getElementById('backbutton')
+
+document.getElementsByTagName('body')[0].style.paddingTop = window.innerHeight / 15 + "px"
+//title.style.paddingTop = window.innerHeight / 20 + "px"
+//title.style.paddingBottom = "0px"
+//title.style.fontSize = window.innerHeight / 10 + "px"
 
 
-title.style.paddingTop = window.innerHeight / 10 + "px"
-title.style.paddingBottom = window.innerHeight / 20 + "px"
-title.style.fontSize = window.innerHeight / 10 + "px"
+backbutton.style.width = window.innerWidth - window.innerHeight / 16 + "px"
+backbutton.style.height = window.innerHeight / 15 + "px"
+backbutton.style.fontSize = window.innerHeight / 20 + "px"
+backbutton.style.marginBottom = 1 + "px"
 
+backbutton.style.top = window.innerHeight / 10 / 2 + "px"
+backbutton.style.top = "10px"
